84f2531c98d8f65e12f9a2af4e2d4c755404cb31
Gennady <gesta@tut.by>
2015-03-15 19:17:04 +0300

    Added JSON.

--- chat/scripts.js
+++ chat/scripts.js
@@ -1,33 +1,109 @@
 var editFlag = false;
 var oldTextPlace;
 var connectionFlag = true;
+var username = 0;
+var messageList = [];
+var tmpMsgEdit;
+
+var theMessage = function(text, user, changeable, date){
+    return {
+        messageText: text,
+        messageUser: user,
+        messageFlag: changeable,
+        messageDate: date
+    };
+};
+
+function run() {
+    username = restoreUsername() || 0;
+    if (username != 0){
+        var tmp = document.getElementById('nameField');
+        tmp.value = username;
+    }
+    messageList = restoreMessages() || [theMessage('Hi, welcome to our chat!','System',false,getTempDate())];
+    fillMessageArea(messageList);
+}
+
+function restoreUsername(){
+    if(typeof(Storage) == "undefined") {
+        alert('Local Storage is not accessible');
+        return;
+    }
+
+    var item = localStorage.getItem("Username");
+
+    return item && JSON.parse(item);
+}
+
+function restoreMessages(){
+    if(typeof(Storage) == "undefined") {
+        alert('Local Storage is not accessible');
+        return;
+    }
+
+    var item = localStorage.getItem("Message List");
+
+    return item && JSON.parse(item);
+}
+
+function store(listToSave) {
+    if(typeof(Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return;
+    }
+
+    localStorage.setItem("Message List", JSON.stringify(listToSave));
+}
+
+function fillMessageArea(messageList){
+    for (var i = 0; i < messageList.length; i++){
+        if (messageList[i].messageFlag == true){
+            insertUserMessage(messageList[i].messageUser, messageList[i].messageText, messageList[i].messageDate)
+        }
+        else {
+            insertMessage(messageList[i].messageUser, messageList[i].messageText, messageList[i].messageDate)
+        }
+    }
+}
 
 function onNameButtonClick() {
-    var username;
-    username = document.getElementById('nameField');
-    changeName(username.value)
+    var tmp = document.getElementById('nameField');
+    if (tmp.value == username){
+        return;
+    }
+    changeName(tmp.value);
 }
 
 function changeName(value){
     if(!value){
         return;
     }
-    var name = document.getElementById('userNameField');
     renameDeclaration(value);
-    name.innerHTML = value;
+    username = value;
+    localStorage.setItem("Username", JSON.stringify(value));
 }
 
 function renameDeclaration(value){
-    if (document.getElementById('userNameField').innerHTML == '') {
-        insertMessage("System",  (value + " has joined this chat."));
+    if (username == 0) {
+        insertMessage("System",  (value + " has joined this chat."), 0);
     }
     else {
-        insertMessage("System",(document.getElementById('userNameField').innerHTML + " has changed name to " + value));
+        insertMessage("System",(username + " has changed name to " + value), 0);
     }
 }
 
+function check(msg){
+    for (var i = 0; i < messageList.length; i++){
+        if (msg.messageUser == messageList[i].messageUser && msg.messageDate == messageList[i].messageDate &&
+            msg.messageFlag == messageList[i].messageFlag && msg.messageText == messageList[i].messageText){
+            return false;
+        }
+    }
+    return true;
+}
+
 function sendMessage(){
-    var userName = document.getElementById('userNameField').innerHTML;
+    var userName = username;
     if (userName == ''){
         window.alert('You have to sign up at first!');
         return;
@@ -40,23 +116,31 @@ function sendMessage(){
     if (newMessage.value == '') {
         return;
     }
-    insertUserMessage(userName, newMessage.value);
+    insertUserMessage(userName, newMessage.value, 0);
     newMessage.value = '';
 }
 
-function insertUserMessage(userName, newMessage){
+function insertUserMessage(userName, newMessage, date){
     var message = document.createElement('tr');
     message.classList.add('userMessageField');
     var messageFunctions = userFunctionsForming();
     var messageName = nameMessageForming(userName);
     var messageText = messageTextForming(newMessage);
-    var date = getTempDate();
+    if (date == 0) {
+        date = getTempDate();
+    }
+    var d = dateForming(date);
     message.appendChild(messageFunctions);
     message.appendChild(messageName);
     message.appendChild(messageText);
-    message.appendChild(date);
+    message.appendChild(d);
+    var msg = theMessage(newMessage,userName,true,date);
+    if (check(msg) == true) {
+        messageList.push(msg);
+    }
     var destination = document.getElementById('chat');
     destination.appendChild(message);
+    store(messageList);
 }
 
 function userFunctionsForming (){
@@ -88,10 +172,16 @@ function messageTextForming(newMessage) {
     return messageText;
 }
 
-function getTempDate() {
+function dateForming(dat){
     var date = document.createElement('td');
+    date.innerHTML = dat;
+    return date;
+}
+
+function getTempDate() {
+    var date;
     var currentDate = new Date();
-    date.innerHTML =  currentDate.getHours() + ":"
+    date =  currentDate.getHours() + ":"
     + currentDate.getMinutes() + ":"
     + currentDate.getSeconds() + " "
     + currentDate.getDate() + "."
@@ -102,22 +192,39 @@ function getTempDate() {
 
 function deleteMessage(thisButton){
     var parent = thisButton.parentNode.parentNode;
+    var place = thisButton.parentNode.parentNode.childNodes.item(2);
+    for (var i = 0; i < messageList.length; i++){
+        if (messageList[i].messageFlag != false){
+            if (messageList[i].messageText == place.innerHTML){
+                messageList.splice(i,1);
+            }
+        }
+    }
+    store(messageList);
     parent.parentNode.removeChild(parent);
 }
 
-function insertMessage(userName, newMessage){
+function insertMessage(userName, newMessage, date){
     var message = document.createElement('tr');
     message.classList.add('messageField');
     var messageFunctions = FunctionsForming();
     var messageName = nameMessageForming(userName);
     var messageText = messageTextForming(newMessage);
-    var date = getTempDate();
+    if (date == 0) {
+        date = getTempDate();
+    }
+    var d = dateForming(date);
     message.appendChild(messageFunctions);
     message.appendChild(messageName);
     message.appendChild(messageText);
-    message.appendChild(date);
+    message.appendChild(d);
+    var msg = theMessage(newMessage,userName,false,date);
+    if (check(msg) == true) {
+        messageList.push(msg);
+    }
     var destination = document.getElementById('chat');
     destination.appendChild(message);
+    store(messageList);
 }
 
 function FunctionsForming (){
@@ -132,6 +239,13 @@ function editButtonClicked(button){
     editFlag = true;
     oldTextPlace = button.parentNode.parentNode.childNodes.item(2);
     document.getElementById('textInput').value = oldTextPlace.innerHTML;
+    for (var i = 0; i < messageList.length; i++){
+        if (messageList[i].messageFlag != false){
+            if (messageList[i].messageText == oldTextPlace.innerHTML){
+                tmpMsgEdit = messageList[i];
+            }
+        }
+    }
 }
 
 function editMessage(){
@@ -141,7 +255,9 @@ function editMessage(){
     }
     editFlag = false;
     oldTextPlace.innerHTML = document.getElementById('textInput').value;
+    tmpMsgEdit.messageText = document.getElementById('textInput').value;
     document.getElementById('textInput').value = '';
+    store(messageList);
 }
 
 function changeIcon(icon){
